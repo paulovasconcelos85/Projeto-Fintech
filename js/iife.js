16aa23162f72d640c1846e45a74f9720166b0452
Paulo Roberto Pessoa Vasconcelos <paulovasconcelos85@Paulos-MacBook-Air.local>
2025-06-17 23:38:43 -0400

    Remove arquivos e pastas desnecessários

--- js/iife.js
+++
@@ -1,5 +0,0 @@
-const calcularValores = (valor1, valor2) => {
-  console.log(`Resultado da Soma: ${valor1 + valor2}`);
-  console.log(`Resultado da Multiplicação: ${valor1 * valor2}`);
-}
-calcularValores(20,10);
--- js/iife2.js
+++
@@ -1,9 +0,0 @@
-(() => {
-const calcularValores = (valor1, valor2) => {
-  console.log(`Resultado da Soma: ${valor1 + valor2}`);
-  console.log(`Resultado da Multiplicação: ${valor1 * 
-  valor2}`);
-  console.log(`Sem escopo global – não são acessadas`);
-}
-calcularValores(30, 30);
-})();
